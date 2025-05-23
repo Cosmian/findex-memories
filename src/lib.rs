89b19915c73041772e24b132e3565e1296cc3d8b
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-05-23 14:27:40 +0200

    feat: first working build

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
-// TODO: check these
-#![warn(clippy::all, clippy::nursery, clippy::cargo)]
-// This is necessary since CryptoCore depends on pkcs8 which depends on an old
-// version of rand_core, which depends on an old version of getrandom (0.2.15),
-// while CryptoCore also depends on gensym which depends on uuid, which depends
-// on a newer version of getrandom (0.3.2).
-#![allow(clippy::multiple_crate_versions)]
+// #[cfg(not(any(
+//     feature = "redis-mem",
+//     feature = "sqlite-mem",
+//     feature = "postgres-mem"
+// )))]
+// panic!(
+//     "You must enable at least one database backend feature: redis-mem, sqlite-mem, or postgres-mem.\n\
+//     Example (cargo.toml):\n\
+//     [dependencies]\n\
+//     cosmian_findex_memories = { version = "1.0", features = [\"redis-mem\"] }\n\""
+// );
 
 #[cfg(feature = "redis-mem")]
-pub use memory::{RedisMemory, RedisMemoryError};
+mod redis_mem;
+#[cfg(feature = "redis-mem")]
+pub use redis_mem::{RedisMemory, RedisMemoryError};
 
 #[cfg(feature = "sqlite-mem")]
-pub use memory::{SqliteMemory, SqliteMemoryError, FINDEX_TABLE_NAME};
+mod sqlite_mem;
+#[cfg(feature = "sqlite-mem")]
+pub use sqlite_mem::{SqliteMemory, SqliteMemoryError, FINDEX_TABLE_NAME};
 
 #[cfg(feature = "postgres-mem")]
-pub use memory::{PostgresMemory, PostgresMemoryError};
+mod postgresql_mem;
+#[cfg(feature = "postgres-mem")]
+pub use postgresql_mem::{PostgresMemory, PostgresMemoryError};
